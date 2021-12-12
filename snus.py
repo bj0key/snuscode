55d7e4f739376998ae5771a951f5353c1460cc14
lew <lewhobnob03@gmail.com>
2021-12-12 03:52:47 +0000

    Refactor old interactive prompt, add support for revision 2 snuscode

--- snus.py
+++ snus.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
 
-USAGE_MSG = """Usage: [snus/unsnus] [message]
-\"tank you\" to exit"""
+
+HELP_REMINDER = "type \"hjelp\" for help"
+EXIT_REMINDER = "type \"tank you\" to exit"
+INVALID_CMD = "ERROR: Invalid Command!"
+INVALID_USAGE = "ERROR: Unexpected number of args given!"
 
 
+func_dict = {}
+func_descriptions = {}
 
-def snus(raw):
+
+def snus_v1(raw):
+    """encode to v1 snuscode"""
     enc = ""
     for c in raw:
         # For each char, encode into a 5-digit ternary 'number'
@@ -17,9 +24,13 @@ def snus(raw):
             w += "snu"[r]
             n = d
         enc += w[::-1]
-    print(enc)
+    return(enc)
+func_dict["snus"]=snus_v1
+func_descriptions["snus"]="encode to v1 snuscode"
 
-def unsnus(enc):
+
+def unsnus_v1(enc):
+    """decode from v1 snuscode"""
     raw = ""
     chunks = [str(enc[i:i+5]) for i in range(0, len(enc), 5)]
     for c in chunks:
@@ -32,35 +43,74 @@ def unsnus(enc):
                 print("ERROR: Invalid encoding! Unable to unsnus")
                 return
         raw += chr(n)
-    print(raw)
+    return(raw)
+func_dict["unsnus"]=unsnus_v1
+func_descriptions["unsnus"]="decode from v1 snuscode"
+
+
+def snus_v2(raw):
+    """encode to v2 snuscode"""
+    enc_int = sum([ord(c)<<(i*7) for i,c in enumerate(raw[::-1])])
+    enc = ""
+    while enc_int != 0:
+        enc_int, rem = divmod(enc_int, 3)
+        enc += "snu"[rem]
+    return enc
+func_dict["neosnus"]=snus_v2
+func_descriptions["neosnus"]="encode to v2 snuscode"
+
+
+def unsnus_v2(enc):
+    """decode from v2 snuscode"""
+    try:
+        enc_int = sum([int("snu".index(c) * int(3**i)) for i,c in enumerate(enc)])
+    except IndexError:
+        print("ERROR: Invalid encoding! Unable to unsnus")
+        return
+    msg_b = bin(enc_int)[2:]
+    chr_b = [msg_b[i:i+7] for i in range(0,len(msg_b),7)]
+    msg = "".join([chr(int(c,2)) for c in chr_b])
+    return msg
+func_dict["unneosnus"]=unsnus_v2
+func_descriptions["unneosnus"]="decode from v2 snuscode"
+
+
+def print_help(*_):
+    for func, desc in func_descriptions.items():
+        print(func.ljust(12) + " - " + desc )
+func_dict["hjelp"]=print_help
+func_descriptions["hjelp"]="show help for this script"
+
 
 def interactive_snus():
-    print("snuscode interactive prompt")
+    print("snuscode interactive prompt v2.0")
     print(USAGE_MSG)
     while(True):
-        while (inp:=input(">>> ").split(" "))[0] not in ["snus", "unsnus"] and " ".join(inp[0:2])!="tank you":
-            print("invalid command!")
-            print(USAGE_MSG)
-        if inp[0]=="help":
-            print(USAGE_MSG)
-        elif inp[0]=="snus":
-            snus(" ".join(inp[1:]))
-        elif inp[0]=="unsnus":
-            unsnus(" ".join(inp[1:]))
-        elif " ".join(inp[0:2]) == "tank you":
-            return
+        inp=input(">>> ").split(" ")
+        cmd,msg = inp[0], " ".join(inp[1:])
+        
+        if " ".join(inp[0:2]) == "tank you": return
+        try:
+            if(ret:=func_dict[cmd](msg)) != None:
+                print(ret)
+        except KeyError:
+            print(INVALID_CMD)
+            print(HELP_REMINDER)
+            print(EXIT_REMINDER)
 
-    
+
 def main():
     if len(argv) == 1:
         interactive_snus()
-    elif len(argv) > 2 and argv[1] == "snus":
-        snus(" ".join(argv[2:]))
-    elif len(argv) > 2 and argv[1] == "unsnus":
-        unsnus(" ".join(argv[2:]))
     else:
-        print("ERROR: Invalid args given!")
-        print(f"Usage: {argv[0]} [snus/unsnus] [msg]")
+        cmd, msg = argv[1], " ".join(argv[2:])
+        try:
+            if(ret:=func_dict[cmd](msg)) != None:
+                print(ret)
+        except KeyError:
+            print(INVALID_CMD)
+            print(f"usage: {argv[0]} <cmd> <msg>")
+            print(HELP_REMINDER)
     
 if __name__ == "__main__":
     main()
